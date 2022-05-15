91f29e905d65f80f78af7a0c3271b448c7752564
Dawid Tyburski <Dawid.Tyburski@microsoft.wsei.edu.pl>
2022-05-15 14:12:06 +0200

    Dodaj pliki projektów.

---
+++ lab_05/ObservableList1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_05
+{
+    public class ObservableList1<T>
+    {
+        
+        private List<T> objects = new List<T>();
+
+        public int Length()
+        {
+            if (objects.Count > 0)
+            {
+                return objects.Count;
+            }
+            else return 0;
+        }
+        
+        public void Add(T param)
+        {
+            objects.Add(param);
+            Console.WriteLine($"{param} added");
+
+        }
+        public T Get(int index)
+        {
+            if (index < objects.Count)
+            {
+                return objects[index];
+            }
+            else return default(T);
+        }
+        public void Set(string index, string newValue)
+        {
+            objects[Int32.Parse(index)] = (T)Convert.ChangeType(newValue, typeof(T));
+        }
+        public void RemoveAt(int index)
+        {
+            List<T> obj = new List<T>();
+
+            for(int j = 0; j< objects.Count; j++)
+            {
+                 if (j != index)
+                 {
+                     obj.Add(objects[j]);
+                 }
+                 else continue;
+            }
+            objects = obj;                
+        }
+    }
+}
---
+++ lab_05/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace lab_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ObservableList1<int> ob1 = new ObservableList1<int>();
+            ob1.Add(1);
+            ob1.Add(3);
+            ob1.Add(5);
+            ob1.Add(7);
+            ob1.Add(9);
+            ob1.Add(11);
+
+            if(ob1.Length()>0)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"ob1 Length: {ob1.Length()}");
+            }
+            while(ob1.Length()>0)
+            {
+                Console.WriteLine();
+                Console.WriteLine("For Get() type get [index]");
+                Console.WriteLine("For Set() type set [index] to [new value]");
+                Console.WriteLine("For RemoveAt() type remove [index]");
+
+                string rl = Console.ReadLine();
+                if(rl != null)
+                {
+                    string[] rlList = rl.Split(" ");
+                    
+                    foreach (string s in rlList)
+                    {
+                        if(s == "get")
+                        {
+                            Console.WriteLine(ob1.Get(Int32.Parse(rlList.Last())));
+                        }
+                        else if(s == "set")
+                        {
+                            ob1.Set(rlList[1], rlList.Last());
+                        }
+                        else if (s == "remove")
+                        {
+                            ob1.RemoveAt(Int32.Parse(rlList.Last()));
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
